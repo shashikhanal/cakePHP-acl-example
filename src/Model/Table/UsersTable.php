f357aa630229d7e3fbf2bfb23a53ad65db28f502
shashikhanal <khanalshashi@gmail.com>
2017-06-06 15:58:15 +0545

    Initial commit

---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,110 @@
+<?php
+namespace App\Model\Table;
+
+use ArrayObject;
+use Cake\Event\Event;
+use Cake\ORM\Entity;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Cake\Auth\DefaultPasswordHasher;
+
+/**
+ * Users Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $Groups
+ *
+ * @method \App\Model\Entity\User get($primaryKey, $options = [])
+ * @method \App\Model\Entity\User newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\User[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\User|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options =[])
+ * @method \App\Model\Entity\User[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\User findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class UsersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     *
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('users');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->addBehavior('Acl.Acl', ['type' => 'requester']);
+
+        $this->belongsTo(
+            'Groups',
+            [
+                'foreignKey' => 'group_id',
+                'joinType'   => 'INNER',
+            ]
+        );
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     *
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('username', 'create')
+            ->notEmpty('username')
+            ->add('username', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
+
+        $validator
+            ->requirePresence('password', 'create')
+            ->notEmpty('password');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     *
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['username']));
+        $rules->add($rules->existsIn(['group_id'], 'Groups'));
+
+        return $rules;
+    }
+
+    public function beforeSave(
+        Event $event,
+        Entity $entity,
+        ArrayObject $options
+    ) {
+        $hasher           = new DefaultPasswordHasher;
+        $entity->password = $hasher->hash($entity->password);
+
+        return true;
+    }
+}
